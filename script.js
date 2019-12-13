9b38b313bff0744e371b930aa67c7c6b221c1d24
Matt Jankowski <mjanko5@uic.edu>
2019-12-13 17:09:38 +0000

    committing at 11

---
+++ script.js
@@ -0,0 +1,53 @@
+      window.mdc.autoInit();
+      // mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer')); 
+      const drawer = document.querySelector('.mdc-drawer').MDCDrawer;
+      // console.log(drawer);
+      const modalTransaction = document.querySelector('#modalTransaction').MDCDialog;
+    
+      // open drawer on nav icon
+      document.getElementsByClassName("mdc-top-app-bar__navigation-icon")[0].addEventListener("click", function(){
+        drawer.open = true;
+      });
+      
+      
+      // close drawer on selection
+//       let navs = document.getElementsByClassName("mdc-list-item");
+      
+//       for (let i = 0; i < navs.length; i++) {
+//         navs[i].addEventListener("click", function() {
+//           drawer.open = false;
+//         })
+//       };
+        
+        $("nav.mdc-list a.mdc-list-item").on("click", function(e) {
+           console.log($(this))
+           let file = "page-content/" + $(this).attr("href").replace("#","") + ".html";
+           $("#content").load(file);
+           drawer.open = false;
+            
+            
+            
+        });
+        
+        $('[data-mdc-dialog-action="yes"]').on("click", function(e) {
+            let url = apiEndpoint + "transactions";
+            let requestBody = {
+                "account": targetAccountId,
+                "symbol": $("#symbol").val(),
+                "shares": $("#shares").val(),
+                "type": $("input[name='transactionType']:checked").val()
+            }
+            $.ajax({
+                    method: "POST",
+                    contentType: "application/json",
+                    url: url,
+                    data: JSON.stringify(requestBody)
+                  })
+                    .done(function(response) { 
+                        console.log(response);
+                    });         
+            
+            
+        })
+
+        
